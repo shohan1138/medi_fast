e9a0c4cc62dd678ea6c75058169baba86d1f74db
shohan1138 <shohanurrahman1138@gmail.com>
2026-08-31 17:36:46 +0600

    fixed cors for live frontend

--- main.py
+++ main.py
@@ -19,11 +19,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","https://medi-fast-mmr9.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
-
 )
 
 
